c35f9c8bd2dd40a07b425da3db9f38706b05a3b9
jamshed45 <jamshed76@gmail.com>
2025-08-12 22:02:36 +0500

    admin update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -11,46 +11,46 @@ class DashboardController extends Controller
     public function index()
     {
 
-        $userRole = auth()->user()->getRoleNames()->first();
-        $user_id = auth()->id();
-        $user = User::findOrFail($user_id);
+        // $userRole = auth()->user()->getRoleNames()->first();
+        // $user_id = auth()->id();
+        // $user = User::findOrFail($user_id);
 
-        if($userRole == 'admin')
-        {
-            $counts = [
-                'organizations' => Organization::count(),
-                'internals' => OrganizationEmployee::count(),
-                'invitations' => Invitation::count(),
-                'inventories' => Inventory::count(),
-            ];
-        }
-        elseif($userRole == 'organization')
-        {
-            $user->organization->id;
-            $counts = [
-                'internals' => OrganizationEmployee::where('organization_id', $user->organization->id)->count(),
-                'invitations' => Invitation::where('organization_id', $user->organization->id)->count(),
-                'inventories' => Inventory::where('organization_id', $user->organization->id)->count(),
-            ];
-        }
-        elseif($userRole == 'subscriber')
-        {
-            // $user->subcriber->id;
-            // $counts = [
-            //     'requests' => Organization::where('organization_id', $user->subcriber->id)->count(),
-            //     'donation_history' => OrganizationEmployee::where('organization_id', $user->subcriber->id)->count(),
-            // ];
+        // if($userRole == 'admin')
+        // {
+        //     $counts = [
+        //         'organizations' => Organization::count(),
+        //         'internals' => OrganizationEmployee::count(),
+        //         'invitations' => Invitation::count(),
+        //         'inventories' => Inventory::count(),
+        //     ];
+        // }
+        // elseif($userRole == 'organization')
+        // {
+        //     $user->organization->id;
+        //     $counts = [
+        //         'internals' => OrganizationEmployee::where('organization_id', $user->organization->id)->count(),
+        //         'invitations' => Invitation::where('organization_id', $user->organization->id)->count(),
+        //         'inventories' => Inventory::where('organization_id', $user->organization->id)->count(),
+        //     ];
+        // }
+        // elseif($userRole == 'subscriber')
+        // {
+        //     // $user->subcriber->id;
+        //     // $counts = [
+        //     //     'requests' => Organization::where('organization_id', $user->subcriber->id)->count(),
+        //     //     'donation_history' => OrganizationEmployee::where('organization_id', $user->subcriber->id)->count(),
+        //     // ];
 
-            // $user->subcriber->id;
-            $counts = [
-                'requests' => 1,
-                'donation_history' => 2,
-            ];
-        }
+        //     // $user->subcriber->id;
+        //     $counts = [
+        //         'requests' => 1,
+        //         'donation_history' => 2,
+        //     ];
+        // }
 
 
         //return view('admin.dashboard.index');
-         return view('admin.dashboard.index', compact('counts', 'userRole'));
+        //  return view('admin.dashboard.index', compact('counts', 'userRole'));
 
     }
 
